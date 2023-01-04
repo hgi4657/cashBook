c28904a825556dfeeed0c12ffab1d7e29a1e4030
lsm <zzpk4175@gmail.com>
2023-01-04 20:27:26 +0900

    연습

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -1,40 +1,96 @@
-const date = new Date();
+let date = new Date();
 
-const Year = date.getFullYear();
-const Month = date.getMonth();
+/** 달력 만들기 함수 */
+function calender() {
+    // 연도 가져오기
+    const Year = date.getFullYear();
+    // 월 가져오기
+    const Month = date.getMonth();
 
-document.querySelector('.year-month').textContent = `${Year}년 ${Month + 1}월`;
+    document.querySelector('.year-month').textContent = `${Year}년 ${Month + 1}월`;
 
-// 이전 달 마지막 날짜
-const prevLast = new Date(Year, Month, 0);
-// 이번 달 마지막 날
-const thisLast = new Date(Year, Month + 1, 0);
+// 현재 날짜의 이전 달 마지막 날짜
+    const prevLast = new Date(Year, Month, 0);
+// 이번 달 마지막 날짜
+    const thisLast = new Date(Year, Month + 1, 0);
+
+// 이전달 마지막 일
+    const PLDate = prevLast.getDate();
+// 이전달 마지막 요일 (월 = 1, 화 = 2, 수 = 3, 목 = 4, 금 = 5, 토 = 6, 일 = 7 )
+    const PLDay = prevLast.getDay();
+
+// 현재 마지막 일
+    const TLDate = thisLast.getDate();
+// 현재 마지막 요일 (월 = 1, 화 = 2, 수 = 3, 목 = 4, 금 = 5, 토 = 6, 일 = 7 )
+    const TLDay = thisLast.getDay();
+
+// 이전 달의 일 리스트(배열)
+    const prevDates = [];
+
+// 이번 달 일자 리스트(배열) 0(key-순서) : 1(value - 일자)
+    const thisDates = [...Array(TLDate + 1).keys()].slice(1);
+
+// 다음 달의 일 리스트(배열)
+    const nextDates = [];
 
 
-const PLDate = prevLast.getDate();
-const PLDay = prevLast.getDay();
-
-const TLDate = thisLast.getDate();
-const TLDay = thisLast.getDay();
-
-const prevDates = [];
-const thisDates = [...Array(TLDate + 1).keys()].slice(1);
-const nextDates = [];
-
-if (PLDay !== 6) {
-    for (let i = 0; i < PLDay; i++) {
-        prevDates.unshift(PLDate - i);
+    if (PLDay !== 6) {
+        for (let i = 0; i <= PLDay; i++) {
+            prevDates.unshift(PLDate - i);
+        }
     }
+
+    for (let i = 1; i < 7 - TLDay; i++) {
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates, nextDates);
+
+    dates.forEach((date, i) => {
+        dates[i] = `<div class="date">${date}</div>`;
+    });
+
+// date div 추가하기
+    document.querySelector('#dates').innerHTML = dates.join('');
+
 }
 
-for (let i = 1; i < 7 - TLDay; i++) {
-    nextDates.push(i);
-}
+// 달력 만들기 함수 호출
+calender();
 
-const dates = prevDates.concat(thisDates, nextDates);
+// ================================================================= //
 
-dates.forEach((date, i) => {
-    dates[i] = `<div class="date">${date}</div>`;
-});
+// 이전 달 버튼
+const prevMonth = document.getElementsByClassName('prev-btn');
+// 다음 달 버튼
+const nextMonth = document.getElementsByClassName('next-btn');
 
-document.querySelector('#dates').innerHTML = dates.join('');
+// 지난 달로 이동
+prevMonth.item(0).addEventListener('click', () => {
+    date.setDate(1);
+    date.setMonth(date.getMonth() - 1);
+    calender();
+})
+
+// 다음 달로 이동
+nextMonth.item(0).addEventListener('click', () => {
+    date.setDate(1);
+    date.setMonth(date.getMonth() + 1);
+    calender();
+})
+
+// ================================================================= //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/resources/static/js/page_re.js
+++ src/main/resources/static/js/page_re.js
@@ -4,7 +4,7 @@ const 한달사용리스트 = document.getElementById("한달사용리스트");
 const 위시리스트 = document.getElementById("위시리스트");
 const 고정지출목록 = document.getElementById("고정지출목록");
 
-// 클릭 시 해당 내역 가져오기
+// 클릭 시 해당 div 열기
 for (let i = 0; i < cont.length; i++) {
     cont.item(i).addEventListener('click', () => {
         const nav_name = document.getElementById(cont.item(i).getAttribute("name"));
@@ -12,11 +12,21 @@ for (let i = 0; i < cont.length; i++) {
             한달사용리스트.style.display = 'none';
             위시리스트.style.display = 'none';
             고정지출목록.style.display = 'none';
+            cont.item(0).style.color = '#66648b';
+            cont.item(0).style.backgroundColor = '#c2c2c2';
+            cont.item(1).style.color = '#66648b';
+            cont.item(1).style.backgroundColor = '#c2c2c2';
+            cont.item(2).style.color = '#66648b';
+            cont.item(2).style.backgroundColor = '#c2c2c2';
+            cont.item(i).style.color = '#ffffff';
+            cont.item(i).style.backgroundColor = '#66648b';
             nav_name.style.display = 'block';
         }
-        else if (nav_name.style.display == 'block') {
+        else {
             nav_name.style.display = 'block'
         }
     })
 }
+
+
 
